6e2da0e217ba5a85fbbbe097ac79bf6558117912
Dung Huynh <12520082@gm.uit.edu.vn>
2020-10-26 01:13:08 +0700

    Updates

---
+++ precache-manifest.4ba83095cf5ca2d84c3f52afc4c40881.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf58c1c72d91857020a50b4d532c2e9",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "224699dcc5e0e5aa536f",
+    "url": "/profile/static/css/main.4d36dd80.chunk.css"
+  },
+  {
+    "revision": "66276a5d0e91539bfed3",
+    "url": "/profile/static/js/2.078b9e4c.chunk.js"
+  },
+  {
+    "revision": "224699dcc5e0e5aa536f",
+    "url": "/profile/static/js/main.2b5d36c0.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "9c5a9cef9766eb5791deb8e029cf1f2f",
+    "url": "/profile/static/media/avatar.9c5a9cef.jpg"
+  },
+  {
+    "revision": "d2c74e7b430633770db40a3b975ddecc",
+    "url": "/profile/static/media/iCielVAGRoundedNext-Bold.d2c74e7b.otf"
+  },
+  {
+    "revision": "5cf37d909218a592aa661568c9727270",
+    "url": "/profile/static/media/iCielVAGRoundedNext-DemiBold.5cf37d90.otf"
+  },
+  {
+    "revision": "6faa576bd8cf054aaa8a7a250c1933ff",
+    "url": "/profile/static/media/iCielVAGRoundedNext-Light.6faa576b.otf"
+  },
+  {
+    "revision": "b15969e2dab8d106cbf185ef081ba9f5",
+    "url": "/profile/static/media/iCielVAGRoundedNext-LightItalic.b15969e2.otf"
+  },
+  {
+    "revision": "028be64712f87065750bcc96cd6ab1b4",
+    "url": "/profile/static/media/iCielVAGRoundedNext-Medium.028be647.otf"
+  },
+  {
+    "revision": "5ea7b2a9b6b7dd2a4f701cf9220c986b",
+    "url": "/profile/static/media/iCielVAGRoundedNext-Regular.5ea7b2a9.otf"
+  }
+]);
